03d1a239058fdfad37b8c8da9621f74c05d9cbec
Jacob Mee <jacobmee@gmail.com>
2024-02-22 03:41:30 +0000

    First version

--- homework_1.cpp
+++ homework_1.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 using namespace std;
 
-const int dayNum[] = {-1,31,29,31,30,31,30,31,31,30,31,30,31};
-int m,offset,i,n;
-ghh
+const int dayNum[] = {-1, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+int m, offset, i, n;
 int main()
 {
-    cin >> m;
-    cin >> n;
-    cout << "S\tM\tT\tW\tT\tF\tS" << endl;
-                offset = n;
-    for(i = 1; i < m; i++)
-                offset = (offset+dayNum[i])%7;
-    for(i = 0; i < offset; i++)
+        m = 1;
+        n = 2;
+        cout << "S\tM\tT\tW\tT\tF\tS" << endl;
+        offset = n;
+        for (i = 1; i < m; i++)
+                offset = (offset + dayNum[i]) % 7;
+        for (i = 0; i < offset; i++)
                 cout << '\t';
-    for(i = 1; i <= dayNum[m]; i++){
+        for (i = 1; i <= dayNum[m]; i++)
+        {
                 cout << i;
-        if (i == dayNum[m] || (offset+i)%7 == 0)
-                cout << endl;
-        else
-                cout << '\t';
-    }
-    
-    return 0;
+                if (i == dayNum[m] || (offset + i) % 7 == 0)
+                        cout << endl;
+                else
+                        cout << '\t';
+        }
+        return 0;
 }
