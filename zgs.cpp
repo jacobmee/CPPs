cab3377beaa880fca8c604b8f82e680347441884
Jacob Mee <jacobmee@gmail.com>
2024-03-16 14:15:38 +0000

    New CPP files

--- zgs.cpp
+++ zgs.cpp
@@ -1,34 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int n;
-    cin >> n;
-    for (int k = 1; k < 10000; k++)
-    {
-        if (n % 2 == 0)
-        {
-            n = n / 2;
-            // cout << n << "    ";
-        }
-        else if (n % 3 == 0)
-        {
-            n = n / 3;
-            // cout << n << "    ";
-        }
-        else if (n % 5 == 0)
-        {
-            n = n / 5;
-            // cout << n << "    ";
-        }
-    }
-    if (n == 1)
-    {
-        cout << "Regular Number"
-             << endl;
-    }
-    else
-        cout << "Irregular Number"
-             << endl;
+int main() {
+  int n;
+  cin >> n;
+
+  while (n % 2 == 0) {
+    n = n / 2;
+  }
+
+  while (n % 3 == 0) {
+    n = n / 3;
+  }
+
+  while (n % 5 == 0) {
+    n = n / 5;
+  }
+
+  if (n == 1) {
+    cout << "Regular Number" << endl;
+  } else
+    cout << "Irregular Number" << endl;
 }
